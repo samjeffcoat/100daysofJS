671ed75ca30b18583d3ac80f2e5b02671f0fa1b4
Sam <samjeffcoat@gmail.com>
2021-05-10 11:46:19 -0500

    Primitive Data Types

---
+++ day1/index.js
@@ -0,0 +1,37 @@
+/*
+We can declare variables in three ways
+*/
+
+var firstName = "Sam";
+let lastName = "Jeffcoat";
+const favoriteFood = "Filet Mignon";
+
+// Primitive Data Types
+// Strings, Number and Boolean
+
+
+// Strings
+const dog = 'fido';
+const string = '2';
+
+// Numbers
+const answer = 5;
+const negative = -13;
+
+// Booleans
+
+const iLoveJavascript = true;
+const isThisaString = false;
+
+// Undefined and Null
+
+console.log(unknownVariable); // undefined
+
+// Null is set by the developer
+
+let phoneNumber = '405-593-5880';
+phoneNumber = null;
+
+console.log(phoneNumber); // null
+
+
